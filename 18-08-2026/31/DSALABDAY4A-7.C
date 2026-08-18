e33831dc166f7af65f0ba1f2a6baeebc54b02942
deyarpan4484-lang <deyarpan4484@gmail.com>
2026-08-18 11:06:41 +0530

    Add files via upload

---
+++ 18-08-2026/31/DSALABDAY4A-7.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+long long count = 0;
+void towerOfHanoi(int n, char source, char auxiliary, char destination)
+{
+    if (n == 1)
+    {
+        printf("Move disc 1 from %c to %c\n", source, destination);
+        count++;
+        return;
+    }
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    printf("Move disc %d from %c to %c\n", n, source, destination);
+    count++;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    towerOfHanoi(n, 'A', 'B', 'C');
+    printf("Total moves: %lld\n", count);
+    return 0;
+}
