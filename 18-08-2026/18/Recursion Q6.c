05d7ad0862a678c564e75f1b9e10a960757d3e11
BIJOY ROY <broy12345joy@gmail.com>
2026-08-18 10:58:27 +0530

    Add files via upload

---
+++ 18-08-2026/18/Recursion Q6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    int mid;
+
+    if (low > high)
+        return -1;
+
+    mid = (low + high) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+
+    if (key < arr[mid])
+        return binarySearch(arr, low, mid - 1, key);
+
+    return binarySearch(arr, mid + 1, high, key);
+}
+
+int main()
+{
+    int n, key, i, result;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted elements:\n");
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter key: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n - 1, key);
+
+    printf("Index = %d", result);
+
+    return 0;
+}
