9bfd7c9854a69252d21617778f8cbec2462a04a6
Arpan Das <dasarpan1596@gmail.com>
2026-08-18 23:16:22 +0530

    Add files via upload

---
+++ 18-08-2026/60/stack_implimentation.c
@@ -0,0 +1,43 @@
+//stack implementation
+#include<stdio.h>
+int stack[20],top=-1;
+void push()
+{
+	if (top==20-1)
+		printf("overflow.");
+	else
+		scanf("%d",&stack[++top],printf("Enter data:"));
+}
+void pop()
+{
+	if(top==-1)
+		printf("\nUnderflow");
+	else
+		printf("\n%d deleted",stack[top--]);
+}
+void disp()
+{
+	int i;
+	if(top==-1)
+		printf("\nEmpty");
+	else
+	{
+		for(i=top;i>=0;i--)
+			printf("\n%d",stack[i]);
+	}
+}
+int main()
+{
+	printf("\n1. push\n2. pop\n3. Disp\n0. Exit");
+	while(1)
+	{
+		int ch;
+		printf("\nEnter choice:");
+		scanf("%d",&ch);
+		if(ch==1) push();
+		else if(ch==2) pop();
+		else if(ch==3) disp();
+		else if(ch==0) break;
+		else printf("\nInvalid choice.");
+	}
+}
---
+++ 18-08-2026/60/summation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+long sumNatural(int n) {
+    if (n <= 0)
+        return 0;
+    return n + sumNatural(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter n:");
+    if (scanf("%d", &n) == 1) {
+        printf("Output: %ld\n", sumNatural(n));
+    }
+    return 0;
+}
