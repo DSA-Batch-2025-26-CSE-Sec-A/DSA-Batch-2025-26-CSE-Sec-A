26f8116f2f08442084996440e55c57a6e9c33ee1
najib-aftab <najibaftab848@gmail.com>
2026-08-18 11:09:56 +0530

    Add files via upload (#677)

---
+++ 18-08-2026/03/question01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    if (n < 0 || n > 20)
+    {
+        printf("Invalid input. Please enter a non-negative integer between 0 and 20.\n");
+        return 1;
+    }
+
+    printf("%lld\n", factorial(n));
+    return 0;
+}
