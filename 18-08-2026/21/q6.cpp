7de6853adf47f835e0d264fc6d0bac02318560ed
manikmaity721635-dot <manikmaity721635@gmail.com>
2026-08-19 13:09:05 +0530

    Add files via upload

---
+++ 18-08-2026/21/q6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(const vector<int>& arr, int left, int right, int key) {
+    if (left > right)
+        return -1;
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+
+    if (key < arr[mid])
+        return binarySearch(arr, left, mid - 1, key);
+
+    return binarySearch(arr, mid + 1, right, key);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cin >> key;
+
+    cout << binarySearch(arr, 0, n - 1, key) << endl;
+
+    return 0;
+}
