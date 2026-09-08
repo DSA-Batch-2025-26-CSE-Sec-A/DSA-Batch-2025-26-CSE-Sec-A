2e135fa7a4055e65a01f367e23036a740a96325a
Sibam Pradhan <sp7309143@gmail.com>
2026-09-08 18:22:03 +0530

    Add files via upload (#1114)

---
+++ 09-01-2026/30/Q2.c
@@ -0,0 +1,33 @@
+//WACP to insert a node at the beginning of a singly linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    struct Node *head = NULL;
+    struct Node *newNode;
+
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    printf("Enter data: ");
+    scanf("%d", &newNode->data);
+
+    newNode->next = head;
+    head = newNode;
+
+    printf("Linked List: ");
+    struct Node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
