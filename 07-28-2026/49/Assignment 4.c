5bf9d4f04d199134c56ebc63b623d903333c7961
Monalisha Dolai <monalishadolai28@gmail.com>
2026-09-11 16:02:44 +0530

    Add C program to reverse an array

---
+++ 07-28-2026/49/Assignment 4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int arr[100],n,i,t;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	printf("\nEnter %d element:",n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n/2;i++)
+	{
+		t=arr[i];
+		arr[i]=arr[n-1-i];
+		arr[n-1-i]=t;
+	}
+	printf("Reverse array:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",arr[i]);
+		printf("\t");
+	}
+	return 0;
+}
