22449282e98aa48d168659d682aec0568ebdfe20
Monalisha Dolai <monalishadolai28@gmail.com>
2026-09-11 16:20:18 +0530

    Implement insertion sort algorithm in C (#1156)

---
+++ 08-04-2026/49/Q8_insertionSort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array that needs to be sorted:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+            arr[j + 1] = key;
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
