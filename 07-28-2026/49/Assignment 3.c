d756923e45183d2abed6ee679e37bacd396f7afc
Monalisha Dolai <monalishadolai28@gmail.com>
2026-09-11 16:00:17 +0530

    Add C program for inserting an element in an array

---
+++ 07-28-2026/49/Assignment 3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int arr[100],n,p,v,i;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	printf("\nEnter %d element:",n);
+	for(i=0;i<=n-1;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("\nEnter position to delete:");
+	scanf("%d",&p);
+	printf("Enter value to insert:");
+	scanf("%d",&v);
+	for(i=n;i>=p;i--)
+	{
+		arr[i]=arr[i-1];
+	}
+	n++;
+	arr[p-1]=v;
+	printf("Array after insertion:");
+	for(i=0;i<n;i++){
+		printf("%d",arr[i]);
+		printf("\t");
+	}
+	return 0;
+}
