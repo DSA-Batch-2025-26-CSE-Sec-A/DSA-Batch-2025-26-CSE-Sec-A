a523900d82e67722b4c6b779063d0f6acd726db2
satarupa133 <satarupamaiti06@gmail.com>
2026-09-16 16:56:12 +0530

    Add files via upload (#1223)

---
+++ 09-15-2026/33/deletion at begining.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    int n, x;
+    Node *head = NULL, *p;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+
+        Node* newNode = new Node{x, NULL};
+
+        if (head == NULL)
+            head = newNode;
+        else {
+            p = head;
+
+            while (p->next != NULL)
+                p = p->next;
+
+            p->next = newNode;
+        }
+    }
+
+    if (head != NULL) {
+        p = head;
+        head = head->next;
+        delete p;
+    }
+
+    cout << "List after deletion: ";
+    p = head;
+
+    while (p != NULL) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+
+    return 0;
+}
