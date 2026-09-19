9a8ccd7ed7433d47558810436d099b6d280ddacd
manikmaity721635-dot <manikmaity721635@gmail.com>
2026-09-19 14:56:37 +0530

    Add files via upload (#1230)

---
+++ 09-15-2026/21/iaan.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    int n, x, key;
+    Node *head = NULL, *p;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+
+        Node* newNode = new Node{x, NULL};
+
+        if (head == NULL)
+            head = newNode;
+        else {
+            p = head;
+
+            while (p->next != NULL)
+                p = p->next;
+
+            p->next = newNode;
+        }
+    }
+
+    cout << "Enter node value and new value: ";
+    cin >> key >> x;
+
+    p = head;
+
+    while (p != NULL && p->data != key)
+        p = p->next;
+
+    if (p != NULL) {
+        Node* newNode = new Node{x, p->next};
+        p->next = newNode;
+    }
+
+    cout << "List: ";
+    p = head;
+
+    while (p != NULL) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+
+    return 0;
+}
