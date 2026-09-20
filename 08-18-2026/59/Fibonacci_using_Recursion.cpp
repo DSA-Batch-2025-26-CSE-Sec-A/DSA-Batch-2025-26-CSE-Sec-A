ac4c9f53f800b14ebd7456590397653f122db483
Simran Mandal <simranmandalwork@gmail.com>
2026-09-20 20:49:05 +0530

    Modify Fibonacci program to display series
    Updated the main function to print the Fibonacci series up to the entered number.

--- 08-18-2026/59/Fibonacci_using_Recursion.cpp
+++ 08-18-2026/59/Fibonacci_using_Recursion.cpp
@@ -12,6 +12,9 @@ int main()
     int n;
     printf("Enter the number ");
     scanf("%d", &n);
-    printf("%d", fibonacci(n));
+    printf("Fibonacci Series: ");
+    for (int i = 0; i <= n; i++) {
+    printf("%d", fibonacci(n));}
+    printf("\t");
     return 0;
 }
